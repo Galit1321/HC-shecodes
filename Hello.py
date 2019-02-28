6d349c78de4f9e03a62021e9de26b3511eacd08b
galit <galit1321@gmail.com>
2019-02-28 21:18:01 +0200

    commit

--- Hello.py
+++ Hello.py
@@ -1,22 +1,28 @@
 import numpy as np
 
+
+class Photo:
+    def __init__(self, p_id, pos,noft, tags):
+        self.noftag=noft
+        self.id = p_id
+        self.pos = pos
+        self.tags = tags
+        self.visit = False
+
+
 def loadInputs(file_name):
     f = open(file_name)
-    content = f.readlines()
-    info = content[0].split()
-    rows = int(info[0])
-    columns = int(info[1])
-    l = int(info[2])
-    h = int(info[3])
-    matrix = np.zeros((rows, columns), dtype=int)
-    content.pop(0)
-    for i in range(rows):
-        for j in range(columns):
-            if content[i][j] == 'M':
-                matrix[i][j] = 1
+    text = f.readlines()
+    N = int(text[0])
+    text.pop(0)
+    res=list([])
+    for i in range(N):
+        row = text[i].split()
+        p=Photo(i,row[0],row[1],row[2:])
+        res.append(p)
+    return res
 
-
-def writeResults(results,file_name):
+def writeResults(results, file_name):
     outputFile = 'output/' + file_name + '.txt'
     f = open(outputFile, 'w')
     f.write(str(len(results)) + '\n')
@@ -24,3 +30,10 @@ def writeResults(results,file_name):
         f.write(str(r[0]) + ' ' + str(r[1]) + ' ' + str(r[2]) + ' ' + str(r[3]) + '\n')
 
     f.close()
+
+
+if __name__ == "__main__":
+    fileFullName = 'a_example.txt'
+    p = loadInputs(fileFullName)
+    for pic in p:
+        print(pic.tags)
